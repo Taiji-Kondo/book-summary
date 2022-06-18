79a4ad91a117fbdf3d4a98d30fca2cc009c6641b
taiji <k.taiji1108@gmail.com>
2022-06-18 15:57:34 +0900

    feat: Generate types

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,24 +1,21 @@
-import { buildConfig } from 'payload/config';
-import path from 'path';
-import Categories from './collections/Categories';
-import Posts from './collections/Posts';
-import Users from './collections/Users';
-import Media from './collections/Media';
+import { buildConfig } from "payload/config";
+import path from "path";
+import Categories from "./collections/Categories";
+import Posts from "./collections/Posts";
+import Users from "./collections/Users";
+import Media from "./collections/Media";
 import Authors from "./collections/Authors";
 
 export default buildConfig({
-  serverURL: 'http://localhost:3001',
+  serverURL: "http://localhost:3001",
   admin: {
     user: Users.slug,
   },
-  collections: [
-    Posts,
-    Users,
-    Media,
-    Authors,
-    Categories,
-  ],
+  collections: [Posts, Users, Media, Authors, Categories],
   typescript: {
-    outputFile: path.resolve(__dirname, 'payload-types.ts')
+    outputFile: path.resolve(
+      __dirname,
+      "../frontend/generated/payload-types.ts"
+    ),
   },
 });

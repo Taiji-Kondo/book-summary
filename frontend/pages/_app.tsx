71a37dd1dbc14fdfe86fc38a0ffa72fab54e5f13
taiji <k.taiji1108@gmail.com>
2022-06-25 15:22:41 +0900

    feat: Setting apollo provider

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  uri: "http://localhost:4000/api/graphql",
+  cache,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 

888df30dc2b252145992e5472fddba52004d95c7
taiji <k.taiji1108@gmail.com>
2022-06-25 15:24:06 +0900

    feat: Setting cors allow api

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
-import express from 'express';
-import payload from 'payload';
+import express from "express";
+import payload from "payload";
+import cors from "cors";
 
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 
+app.use(cors());
+
 // Redirect root to Admin panel
-app.get('/', (_, res) => {
-  res.redirect('/admin');
+app.get("/", (_, res) => {
+  res.redirect("/admin");
 });
 
 // Initialize Payload
@@ -15,10 +18,10 @@ payload.init({
   mongoURL: process.env.MONGODB_URI,
   express: app,
   onInit: () => {
-    payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+    payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
   },
-})
+});
 
 // Add your own express routes here
 
-app.listen(3001);
+app.listen(4000);
